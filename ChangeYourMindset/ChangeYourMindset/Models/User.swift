08f84dc1b832675c72e0149586b0dd97b1a06a4d
Dev <devenlukeday@gmail.com>
2020-10-23 14:26:25 -0600

    Creates complete user model file with extensions

--- ChangeYourMindset/ChangeYourMindset/Models/User.swift
+++ ChangeYourMindset/ChangeYourMindset/Models/User.swift
@@ -5,4 +5,83 @@
 //  Created by Deven Day on 10/23/20.
 //
 
-import Foundation
+import UIKit
+import CloudKit
+
+struct UserStrings {
+    static let recordTypeKey = "User"
+    fileprivate static let usernameKey = "username"
+    static let appleUserReferenceKey = "appleUserReference"
+    fileprivate static let photoAssetKey = "photoAsset"
+}
+
+class User {
+    
+    var username: String
+    var recordID: CKRecord.ID
+    var appleUserReference: CKRecord.Reference
+    var profilePhoto: UIImage? {
+        get {
+            guard let data = photoData else { return nil }
+            return UIImage(data: data)
+        } set {
+            photoData = newValue?.jpegData(compressionQuality: 0.5)
+        }
+    }
+    var photoData: Data?
+    
+    var photoAsset: CKAsset? {
+        get {
+            let tempDirectory = NSTemporaryDirectory()
+            let tempDirectoryURL = URL(fileURLWithPath: tempDirectory)
+            let fileURL = tempDirectoryURL.appendingPathComponent(UUID().uuidString).appendingPathExtension("jpg")
+            do {
+                try photoData?.write(to: fileURL)
+            } catch {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+            }
+            return CKAsset(fileURL: fileURL)
+        }
+    }
+    
+    init(username: String, recordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString), appleUserReference: CKRecord.Reference, profilePhoto: UIImage? = nil) {
+        self.username = username
+        self.recordID = recordID
+        self.appleUserReference = appleUserReference
+        self.profilePhoto = profilePhoto
+    }
+}//END OF CLASS
+//MARK: - Extensions
+extension User {
+    convenience init?(ckRecord: CKRecord) {
+        guard let username = ckRecord[UserStrings.usernameKey] as? String,
+              let appleUserReference = ckRecord[UserStrings.appleUserReferenceKey] as? CKRecord.Reference
+        else { return nil }
+        
+        var foundPhoto: UIImage?
+        
+        if let photoAsset = ckRecord[UserStrings.photoAssetKey] as? CKAsset {
+            do {
+                let data = try Data(contentsOf: photoAsset.fileURL!)
+                foundPhoto = UIImage(data: data)
+            } catch {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+            }
+        }
+        
+        self.init(username: username, recordID: ckRecord.recordID, appleUserReference: appleUserReference, profilePhoto: foundPhoto)
+    }
+}//END OF EXTENSION
+
+extension CKRecord {
+    convenience init(user: User) {
+        self.init(recordType: UserStrings.recordTypeKey, recordID: user.recordID)
+        setValuesForKeys([
+            UserStrings.usernameKey : user.username,
+            UserStrings.appleUserReferenceKey : user.appleUserReference
+        ])
+        if let asset = user.photoAsset {
+            setValue(asset, forKey: UserStrings.photoAssetKey)
+        }
+    }
+}//END OF EXTENSION
