b539e0088b6936f913fa51bd4f21df8f2955b531
Dev <devenlukeday@gmail.com>
2020-10-23 13:22:51 -0600

    creates project and file structure

---
+++ ChangeYourMindset/ChangeYourMindset/Controllers/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChangeYourMindset
+//
+//  Created by Deven Day on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
