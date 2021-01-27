1c874f7e857285fd434b14afe2e6b2e5e4364cda
Dev <devenlukeday@gmail.com>
2021-01-27 14:31:03 -0700

    fixes infoplist information

--- ChangeYourMindset/ChangeYourMindset/Controllers/ViewControllers/OverviewViewController.swift
+++ ChangeYourMindset/ChangeYourMindset/Controllers/ViewControllers/OverviewViewController.swift
@@ -57,8 +57,8 @@ class OverviewViewController: UIViewController {
         collectionView.backgroundColor = .systemRed
         collectionView.isScrollEnabled = false
         collectionView.collectionViewLayout = configureCollectionViewLayout()
-        //        collectionView.addAccentBorder()
-        //        collectionView.layer.cornerRadius = 20
+                collectionView.addAccentBorder()
+                collectionView.layer.cornerRadius = 20
     }
     
     func configureCollectionViewLayout() -> UICollectionViewLayout {
